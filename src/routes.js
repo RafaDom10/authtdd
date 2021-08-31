c2f0ce4d4349d4ca972f13d88412692b237d83c6
RafaDom10 <r-domingues@outlook.com>
2021-08-31 19:45:21 -0300

    config db done

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,6 @@
 const routes = require('express').Router();
+const { User } = require('./app/models');
+
+User.create({ name: 'Ann', email: 'ann@mail.com', password_hash: '12345' });
 
 module.exports = routes;
