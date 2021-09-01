48aa4a625ca65cbc5ff141b00cc4cc09d9ed24fc
RafaDom10 <r-domingues@outlook.com>
2021-08-31 21:58:01 -0300

    tests done

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,13 @@
 const routes = require('express').Router();
-const { User } = require('./app/models');
 
-User.create({ name: 'Ann', email: 'ann@mail.com', password_hash: '12345' });
+const authMiddleware = require('./app/middleware/auth');
+
+const SessionController = require('./app/controllers/SessionController');
+
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.get('/dashboard', (req, res) => res.status(200).send());
 
 module.exports = routes;
